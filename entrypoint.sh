6d88b7f630090fbfc72afda0551338e8e638fa2e
Eugene Ryoo <eugryooene@gmail.com>
2026-08-25 14:32:54 -0400

    use mkdocs native draft func + interhited settings + prod-ish webserver

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# entrypoint.sh
+set -e
+
+if [ "$APP_TAG" = "production" ]; then
+    echo "Starting in Production Mode..."
+    mkdocs build
+    exec caddy run --config Caddyfile --adapter caddyfile
+else
+    echo "Starting in Development Mode..."
+    exec mkdocs serve -f mkdocs.dev.yml -a 0.0.0.0:8000
+fi
