daed32727917536e7117caecc3079f437f87becc
Eugene Ryoo <eugryooene@gmail.com>
2026-08-25 14:41:09 -0400

    adjust entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,7 @@
 set -e
 
 if [ "$APP_TAG" = "production" ]; then
-    echo "Starting in Production Mode..."
-    mkdocs build
-    exec caddy run --config Caddyfile --adapter caddyfile
+    echo "Starting in Production Mode..." && mkdocs build && exec caddy run --config Caddyfile --adapter caddyfile
 else
-    echo "Starting in Development Mode..."
-    exec mkdocs serve -f mkdocs.dev.yml -a 0.0.0.0:8000
+    echo "Starting in Development Mode..." && exec mkdocs serve -f mkdocs.dev.yml -a 0.0.0.0:8000
 fi
